5aa735bf5f1b4e5e720aa3bd4e5c440fdf3ad196
dobkir <dobkir@gmail.com>
2021-07-04 21:55:06 +0400

    How to create Store, Reduser, Hooks: useDispatch and use Selector. An example is adding cash into the bank account and withdrawal from it

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, CashActionTypes, RootState } from './index';
 
-function App() {
+import styles from './App.module.scss';
+
+const App: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const cash: number = useSelector((state: RootState) => state.cash);
+
+  const addCash = (cash: number) => {
+    dispatch({ type: CashActionTypes.ADD_CASH, payload: cash })
+  };
+  const getCash = (cash: number) => {
+    dispatch({ type: CashActionTypes.GET_CASH, payload: cash })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <div className={styles.cashAmount}>
+        Money into your account:<br />{cash}
+      </div>
+      <div className={styles.buttonsArea}>
+        <button onClick={() => addCash(Number(prompt("How much cash do you want to add?")))}
+          className={`${styles.button} ${styles.buttonBlue}`}>
+          ADD CASH
+        </button>
+        <button onClick={() => getCash(Number(prompt("How much cash do you want to get?")))}
+          className={`${styles.button} ${styles.buttonRed}`}>
+          GET CASH
+        </button>
+      </div>
     </div>
   );
 }
